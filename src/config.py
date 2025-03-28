17bf46a1bd0f720bacdc3aa8088e599902233468
Aakash Dilip <aakash.dilip101@gmail.com>
2025-03-28 12:10:02 +1100

    Updated config file to correctly access environment variables depending on the situation

--- src/config.py
+++ src/config.py
@@ -3,8 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-REGION = os.getenv("AWS_REGION")
-POOL_ID = os.getenv("COGNITO_POOL_ID")
-CLIENT_ID = os.getenv("COGNITO_CLIENT_ID")
-DB = os.getenv("DYNAMODB_TABLE")
-CLIENT_SECRET = os.getenv("COGNITO_CLIENT_SECRET")
+ENVIRONMENT = os.environ.get('ENVIRONMENT', 'local')
+
+if ENVIRONMENT == 'testing':
+    POOL_ID = "test-id"
+    CLIENT_ID = "test-id"
+    DB = "test-table"
+    CLIENT_SECRET = "test-secret"
+elif ENVIRONMENT == 'local':
+    POOL_ID = os.environ.get("COGNITO_POOL_ID")
+    CLIENT_ID = os.environ.get("COGNITO_CLIENT_ID")
+    DB = os.environ.get("DYNAMODB_TABLE")
+    CLIENT_SECRET = os.environ.get("COGNITO_CLIENT_SECRET")
+elif ENVIRONMENT == 'production':
+    pass
+    # Insert code to retrieve sensitive values from AWS ECS Secrets Manager
