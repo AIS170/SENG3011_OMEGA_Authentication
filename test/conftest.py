843fd72e28ae94179a182c1aad375e5db3ee1e7f
Aakash Dilip <aakash.dilip101@gmail.com>
2025-04-19 00:37:25 +1000

    Setup access to Cognito correctly

--- test/conftest.py
+++ test/conftest.py
@@ -48,40 +48,6 @@ def test_cognito():
 
 
 @pytest.fixture(scope="function")
-def test_cognito_with_autoverify():
-    client = boto3.client("cognito-idp", region_name=REGION)
-
-    pool = client.create_user_pool(
-        PoolName="testPool",
-        AutoVerifiedAttributes=["email"],
-    )
-    pool_id = pool["UserPool"]["Id"]
-
-    pool_client_response = client.create_user_pool_client(
-        UserPoolId=pool_id,
-        ClientName="testClient",
-        GenerateSecret=True,
-        ExplicitAuthFlows=["USER_PASSWORD_AUTH"]
-    )
-
-    client_id = pool_client_response["UserPoolClient"]["ClientId"]
-    client_secret = pool_client_response["UserPoolClient"].get(
-        "ClientSecret"
-    )
-
-    with patch("src.auth.CLIENT_ID", client_id), \
-         patch("src.auth.CLIENT_SECRET", client_secret), \
-         patch("src.auth.POOL_ID", pool_id), \
-         patch("src.auth.get_cognito", return_value=client):
-        yield {
-            "client": client,
-            "user_pool_id": pool_id,
-        }
-
-    client.delete_user_pool(UserPoolId=pool_id)
-
-
-@pytest.fixture(scope="function")
 def clear_dynamo():
     yield
 
